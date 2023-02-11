11a06a08317d7c6f96fd5503ab592d32f3e1c278
Bhoopesh <bhoopesh459@gmail.com>
2023-02-11 23:36:44 +0530

    Feat:particles js

--- frontend/src/components/Bottom/Bottom.tsx
+++ frontend/src/components/Bottom/Bottom.tsx
@@ -7,7 +7,7 @@ import LearnImage from '../../assets/LearnMore.png';
 const Bottom: React.FC = () => {
   return (
     <div className={styles.container + ' flex flex-col p-10 justify-center items-center'}>
-      <div className='basis-[70%] w-screen flex max-lg:flex-col items-center justify-center gap-[8vw]'>
+      <div className='basis-[70%] w-screen flex max-lg:flex-col items-center justify-center gap-[8vw] z-10'>
         <div className={styles.divEl + ' flex flex-col xl:w-80 xl:h-80 sm:h-64 sm:w-64 rounded-3xl bg-[#262626] justify-center items-center gap-10 '}>
           <img src={RankingImage} alt="logo" />
           <p>Global Rankings</p>
@@ -24,7 +24,7 @@ const Bottom: React.FC = () => {
       <div className='basis-[10%] '>
         <p className='text-xl'>Install our chrome extension and contribute towards a greener society</p>
       </div>
-      <div className={styles.btn + ' flex basis-[20%] items-center justify-center  w-screen font-bold text-green-500'}>
+      <div className={styles.btn + ' flex basis-[20%] items-center justify-center  w-screen font-bold text-green-500 z-10'}>
           <button className='hover:bg-green-500'>Install Now</button>
       </div>
     </div>
--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -1,11 +1,124 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import Bottom from '../Bottom/Bottom';
 import Top from './Top';
 import styles from './home.module.css';
 
-const Home : React.FC = () => {
+import Particles from "react-particles";
+import type { Engine } from "tsparticles-engine";
+import { loadFull } from "tsparticles";
+
+const Home: React.FC = () => {
+
+    const customInit = useCallback(async (engine: Engine) => {
+        // this adds the bundle to tsParticles
+        await loadFull(engine);
+    }, []);
+
+    const options = {
+        /* custom options */
+        fpsLimit: 60,
+        particles: {
+            number: {
+                value: 10,
+                density: {
+                    enable: true,
+                    value_area: 800
+                }
+            },
+            color: {
+                value: "#ffffff"
+            },
+            shape: {
+                type: "polygon",
+                stroke: {
+                    width: 0,
+                    color: "#000000"
+                },
+                polygon: {
+                    nb_sides: 6
+                },
+
+            },
+            opacity: {
+                value: 0.2,
+                random: true,
+                anim: {
+                    enable: true,
+                    speed: 0.2,
+                    sync: false,
+                }
+            },
+            size: {
+                value: 60,
+                random: true,
+                anim: {
+                    enable: false,
+                    speed: 40,
+                    size_min: 1,
+                    sync: false
+                }
+            },
+            move: {
+                enable: true,
+                speed: 6,
+                direction: "none",
+                random: false,
+                straight: false,
+                out_mode: "out",
+                bounce: false,
+                attract: {
+                    enable: false,
+                    rotateX: 600,
+                    rotateY: 1200
+                }
+            },
+            interactivity: {
+                detect_on: "canvas",
+                events: {
+                  onhover: {
+                    enable: true,
+                    mode: "repulse",
+                  },
+                  onclick: {
+                    enable: true,
+                    mode: "push",
+                  },
+                  resize: true,
+                },
+                modes: {
+                  grab: {
+                    distance: 400,
+                    line_linked: {
+                      opacity: 1,
+                    },
+                  },
+                  bubble: {
+                    distance: 400,
+                    size: 40,
+                    duration: 2,
+                    opacity: 8,
+                    speed: 3,
+                  },
+                  repulse: {
+                    distance: 200,
+                    duration: 0.4,
+                  },
+                  push: {
+                    particles_nb: 4,
+                  },
+                  remove: {
+                    particles_nb: 2,
+                  },
+                },
+              },
+              retina_detect: true,
+
+        },
+    };
+
     return (
         <div className={styles.home}>
+            <Particles options={options} init={customInit} />
             <Top />
             <Bottom />
         </div>
