9dc30083a93ced910b7324b8f7e94ddb9d4aeb6b
Deveshwar <deveshwar00@gmail.com>
2023-02-11 18:36:05 +0530

    feat: integrate

--- ChromeExtension/scripts/background.js
+++ ChromeExtension/scripts/background.js
@@ -1,37 +1,63 @@
 // chrome.runtime.sendMessage({ greeting: "hello" }, function (response) {
 //     console.log(response.farewell);
 // });
-let currentSize = 0;
-let c = 0;
+
+const frontendUrl = "http://localhost:3000/";
+const backendUrl = "http://localhost:5000/";
 
 let CurrentVisited = 0;
 
 current_url = "";
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  const messageName = message.name;
+    const messageName = message.name;
 
-  if (messageName === "size") {
-    currentSize += message.bytes;
-    CurrentVisited += 1;
-    current_url = message.baseurl;
-  } else if (messageName === "request") {
-    sendResponse({
-      size: currentSize,
-      visted: CurrentVisited,
-      current_url: current_url,
-    });
-  }
+    if (messageName === "size") {
+        currentSize += message.bytes;
+        CurrentVisited += 1;
+        current_url = message.baseurl;
+    } else if (messageName === "request") {
+        sendResponse({
+            size: currentSize,
+            visted: CurrentVisited,
+            current_url: current_url,
+        });
+    }
 });
 
 chrome.webRequest.onCompleted.addListener(
-  (details) => {
-    details.responseHeaders.forEach((header) => {
-      if (header.name === "content-length") {
-        currentSize += parseInt(header.value);
-      }
-    });
-  },
-  { urls: ["<all_urls>"] },
-  ["responseHeaders"]
+    (details) => {
+        details.responseHeaders.forEach((header) => {
+            if (header.name === "content-length") {
+                currentSize = parseInt(header.value);
+                chrome.cookies.get({
+                    url: frontendUrl,
+                    name: "session",
+                }, async function (cookie) {
+                    const res = await fetch(backendUrl + "/emit", {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                            "Cookie": "session=" + cookie.value,
+                        },
+                        body: JSON.stringify({
+                            url: details.url,
+                            data: currentSize,
+                        }),
+                    });
+                    if(!res.ok){
+                        chrome.cookies.set({
+                            url: frontendUrl,
+                            name: "session",
+                            value: "",
+                        });
+                    }
+                    return res.json();
+                }
+                );
+            }
+        });
+    },
+    { urls: ["<all_urls>"] },
+    ["responseHeaders"]
 );
--- ChromeExtension/scripts/popup.js
+++ ChromeExtension/scripts/popup.js
@@ -1,13 +1,65 @@
 const h1 = document.querySelector("h1");
 const p1 = document.querySelector("p");
 
+const frontendUrl = "http://localhost:3000/";
+const backendUrl = "http://localhost:5000/";
+// chrome.runtime.sendMessage(
+//     { name: "request", userid: "123" },
+//     function (response) {
+//         console.log(response);
+//         h1.textContent = response.size;
+//         p1.textContent = response.change;
+//     }
+// );
+const login = async (email, password) => {
+    const res = await fetch(backendUrl + "/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            email: email,
+            password: password
+        }),
+    });
+    const cookie = res.headers.get("set-cookie");
+    chrome.cookies.set({
+        url: frontendUrl,
+        name: "session",
+        value: cookie.split(";")[0].split("=")[1],
+    });
+};
 
+const register = async (email, password) => {
+    await fetch(backendUrl + "/signup", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            email: email,
+            password: password
+        }),
+    });
+}
 
-chrome.runtime.sendMessage(
-  { name: "request" ,userid: "123"},
-  function (response) {
-    console.log(response);
-    h1.textContent = response.size;
-    p1.textContent = response.change;
-  }
-);
+const getCurrentSessionEmissions = async () => {
+    chrome.cookies.get({
+        url: frontendUrl,
+        name: "session",
+    }, async function async(cookie) {
+        const res = await fetch(backendUrl + "/current", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Cookie": "session=" + cookie.value,
+            },
+        });
+        h1.textContent = res.json();
+    }
+    );
+}
+
+setInterval(() => {
+    getCurrentSessionEmissions();
+}, 30000);
