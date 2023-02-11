26ed07e8523ebf962d30b8004aecebdf01042be7
sriman <kssrisowrirajan@gmail.com>
2023-02-11 21:31:38 +0530

    home top component

--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import styles from './home.module.css';
+import React from "react";
+import styles from "./home.module.css";
+import  Top  from "./Top";
 
 const Home = () => {
-    return (
-        <div className=''>
-            Home
-        </div>
-    )
-}
+  return (
+    <div className={styles.home}>
+      <Top />
+    </div>
+  );
+};
 
 export default Home;
---
+++ frontend/src/components/Home/Top.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import styles from "./top.module.css";
+import icon from "../../assets/icon.svg";
+import tree from "../../assets/tree.svg";
+const Top: React.FC = () => {
+  return (
+    <div className="flex flex-col items-center justify-start h-screen w-screen  bg-black  text-slate-100 p-8 ">
+      <div className={styles.heading}>
+        <img src={icon} alt="icon" draggable="false" />
+        Carbon Meter
+      </div>
+      <div className={styles.subheading}>
+        <p>" Calculate your carbon footprint "</p>
+      </div>
+      <div className="flex items-center ">
+        <div className={"text-left w-2/3 text-2xl " +styles.mainLeft }>
+            <h1
+            className={"text-6xl font-bold pb-8 " +styles.h1}
+            >Did You Know ?</h1>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam
+          dolorem illum veritatis quaerat! Suscipit non, ad perferendis
+          laudantium deserunt voluptatem? Labore omnis quas, odit delectus
+          ducimus dolor quam dignissimos aliquam. Id nulla officia commodi
+          tenetur et quasi ex minima culpa, soluta vero, in iure laborum alias
+          pariatur incidunt! Eligendi aliquam consectetur praesentium totam
+          voluptatem assumenda odit distinctio illo quia mollitia. Commodi quia
+          nisi expedita at deleniti amet culpa nulla rerum tempore adipisci
+          fugit mollitia perferendis aperiam doloribus, recusandae iste a modi
+          iusto impedit? Unde voluptatibus ex placeat ullam voluptas nesciunt?
+        </div>
+        <div className="flex items-center justify-end w-1/3">
+          <img
+            className={styles.img1}
+            src={tree}
+            alt="tree"
+            draggable="false"
+          />
+          <img
+            className={styles.img2}
+            src={tree}
+            alt="tree"
+            draggable="false"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Top;
